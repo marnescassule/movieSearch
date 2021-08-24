88f3015b81bf580251ad3f24db252a2deae924d3
Marnes Adão Simão Cassule <marnes.simao.cassule@avanade.com>
2021-08-24 10:42:35 -0300

    Aplicação Search

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchMovie from "./searchMovie";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovie/>
+    </div>
+  );
+}
+
+export default App;
